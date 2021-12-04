fc3588cb6d3105456ac0a64df313256e94682c3d
Andras Pal <andras.pal@outlook.com>
2021-12-04 20:10:59 +0000

    update the scoreDetailsVM to have default values for the optionals

--- ClearScoreDashboard/ScoreDetailsView/ScoreDetailsVM.swift
+++ ClearScoreDashboard/ScoreDetailsView/ScoreDetailsVM.swift
@@ -18,22 +18,22 @@ class ScoreDetailsVM {
   // # Private/Fileprivate
   private let mainExecutor: Executor
   private let creditReport : CreditReportInfo
-
+  
   // # Public/Internal/Open
   lazy var currentShortTermDebt = {
-    "Current short term debt: " + "\(String(describing: self.creditReport.currentShortTermDebt))"
+    "Current short term debt: " + "\(self.creditReport.currentShortTermDebt ?? 0)"
   }
   lazy var currentShortTermCreditLimit = {
-    "Current long term debt: " + "\(String(describing: self.creditReport.currentShortTermCreditLimit))"
+    "Current long term debt: " + "\(self.creditReport.currentShortTermCreditLimit ?? 0)"
   }
   lazy var currentLongTermDebt = {
-    "Current long term debt: " + "\(String(describing: self.creditReport.currentLongTermDebt))"
+    "Current long term debt: " + "\(self.creditReport.currentLongTermDebt ?? 0)"
   }
   lazy var percentageCreditUsed = {
-    "Percentage credit used: " + "\(String(describing: self.creditReport.percentageCreditUsed))"
+    "Percentage credit used: " + "\(self.creditReport.percentageCreditUsed ?? 0)"
   }
   lazy var daysUntilNextReport = {
-    "Days until next report: " + "\(String(describing: self.creditReport.daysUntilNextReport))"
+    "Days until next report: " + "\(self.creditReport.daysUntilNextReport ?? 0)"
   }
   
   //=======================================
